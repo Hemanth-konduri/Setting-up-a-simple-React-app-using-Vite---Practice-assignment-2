d909c7f0fed30d3fba9160807d8ff5bfa5619d40
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-03-28 13:32:42 +0530

    Completed

---
+++ react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hemanth Konduri</h1>
+     <h2>Github userName: Hemanth-konduri </h2>
+     <h3>E-mail: kondurihemanth62@gmail.com</h3>
+     <h4>Kalvium mail: konduri.hemanth.s89@kalvium.community</h4>
+     <h5>Address: Rajupalem village, kothapatnam mandal, prakasam dist, AP.</h5>
+    </>
+  )
+}
+
+export default App
